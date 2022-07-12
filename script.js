a3ca3091cc4b5d4eff65e39078d4d2519490c777
EDJERROUD <djerroud13000@gmail.com>
2022-07-12 20:37:28 +0200

    ajout css + javascript

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 // TYPED //
-var typed3 = new Typed('.typed3', {
-    strings: ['coucou','salut','comment ça va','wesh'],
-    typeSpeed: 0,
-    backSpeed: 0,
-    smartBackspace: true, // this is a default
-    loop: true
+var typed3 = new Typed('.typed', {
+    strings: ["Bonjour à toutes et à tous, après une carrière de 13 ans dans l'industrie agro-alimentaire.J'ai décidé de me lancer à fond dans l'apprentissage du code, depuis près de 6 mois, je me forme à temps plein aux langages informatique tels que HTML, CSS,JAVASCRIPT, PHP et MYSQL afin de devenir le plus rapidement possible développeur. Plus qu'un avenir professionnel, je me suis découvert une passion, LE CODE ."],
+    typeSpeed: 20,
+   
+    
   });
     
