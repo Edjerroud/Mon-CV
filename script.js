3358a7fc2e50feb28e1b404d91f95222b3dbcd20
EDJERROUD <djerroud13000@gmail.com>
2022-07-12 10:38:57 +0200

    Projet CV

---
+++ script.js
@@ -0,0 +1,9 @@
+// TYPED //
+var typed3 = new Typed('.typed3', {
+    strings: ['coucou','salut','comment ça va','wesh'],
+    typeSpeed: 0,
+    backSpeed: 0,
+    smartBackspace: true, // this is a default
+    loop: true
+  });
+    
