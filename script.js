5823af60e275429d2b0874ce5b21e8f98deca6e1
EDJERROUD <djerroud13000@gmail.com>
2022-07-20 18:39:49 +0200

    suite du projet

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ window.addEventListener('scroll',function(){
 // TYPED //
 var typed3 = new Typed('.typed', {
     strings: ["Bonjour à toutes et à tous, après une carrière de 13 ans dans l'industrie agro-alimentaire.J'ai décidé de me lancer à fond dans l'apprentissage du code, depuis près de 6 mois, je me forme à temps plein aux langages informatique tels que HTML, CSS,JAVASCRIPT, PHP et MYSQL afin de devenir le plus rapidement possible développeur. Plus qu'un avenir professionnel, je me suis découvert une passion, LE CODE ."],
-    typeSpeed: 30,
+    typeSpeed: 10,
    
     
   });
@@ -53,3 +53,5 @@ var typed3 = new Typed('.typed', {
   }
 });
     
+ // AOS /
+ AOS.init();
